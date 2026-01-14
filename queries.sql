1c41124e418bc03097e933568f74d35378d36feb
martinagiammillari <martigiammi@gmail.com>
2026-01-14 15:30:44 +0100

    feat: final commit

--- queries.sql
+++ queries.sql
@@ -55,6 +55,9 @@ WHERE phone IS NULL
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(enrolment_date), COUNT(*)
+FROM students
+GROUP BY YEAR(enrolment_date);
 
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
